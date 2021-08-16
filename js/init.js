4fb8464dbbcdff3eba91fc5580e1e8337b02310e
joacotrinanes <joaco.trinanes@gmail.com>
2021-08-16 17:57:35 -0300

    Deleted Google Access

--- js/init.js
+++ js/init.js
@@ -54,27 +54,14 @@ var nombreRecordado = JSON.parse(localStorage.getItem('usuario'));
 var desconectar = function() {
     localStorage.clear();
     sessionStorage.clear();
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-        console.log('User signed out.');
-    });
-
 };
 
-// Google Access
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-        console.log('User signed out.');
-    });
-};
 
 // Saludar invitado
 
 var saludarInvitado = function() {
 
-    if (nombreNoRecordado == null && nombreRecordado == null || localStorage.isNotNull()) {
+    if (nombreNoRecordado == null && nombreRecordado == null) {
         window.location.href = 'login.html';
 
     } else if (nombreRecordado !== '' && nombreNoRecordado == null) {
