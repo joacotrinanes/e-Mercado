7144c5619b721ef85869f0f2627a2f9102124dd0
joacotrinanes <joaco.trinanes@gmail.com>
2021-08-16 12:00:17 -0300

    Working with Google Access

--- js/login.js
+++ js/login.js
@@ -94,8 +94,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    /* console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+     console.log('Name: ' + profile.getName());
+     console.log('Image URL: ' + profile.getImageUrl());
+     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.*/
+    window.location.href = 'index.html'
+    datos.usuario = profile.getName();
+    datos.contraseña = 'google';
+    datos.estado = 'conectado';
 };
