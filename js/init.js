9534068147631793c147c6ecb213e50d4f79197e
joacotrinanes <joaco.trinanes@gmail.com>
2021-08-17 12:36:15 -0300

    Google Access

--- js/init.js
+++ js/init.js
@@ -77,11 +77,9 @@ function onLoad() {
     });
 };
 
-function onSignIn() {
+function onSignIn(googleUser) {
 
-
-    var googleAuth = gapi.auth2.getAuthInstance();
-    var profile = googleAuth.getBasicProfile();
+    var profile = googleUser.getBasicProfile();
 
     /*console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
