6ed6fa86ef746aae26faf8b55f24738d812e3e18
joacotrinanes <joaco.trinanes@gmail.com>
2021-08-18 15:57:36 -0300

    Google Access

--- js/init.js
+++ js/init.js
@@ -66,7 +66,7 @@ var desconectar = function() {
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
+    auth2.disconnect().then(function() {
         console.log('User signed out.');
     });
 };
