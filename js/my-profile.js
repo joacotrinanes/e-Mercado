4db471f0bbde0911812c09a2079f27c9cef49a86
joacotrinanes <joaco.trinanes@gmail.com>
2021-10-20 14:44:28 -0300

    Profile Image

--- js/my-profile.js
+++ js/my-profile.js
@@ -7,6 +7,8 @@ let lastNames = document.getElementById('apellidosPerfil');
 let age = document.getElementById('edad');
 let email = document.getElementById('emailPerfil');
 let telephone = document.getElementById('telefono');
+let image = document.getElementById('imgPerfil');
+//let imgInBase64 = getBase64Image(image);
 
 /*LocalStorage variables*/
 let savedName = JSON.parse(localStorage.getItem('name'));
@@ -14,6 +16,7 @@ let savedLastNames = JSON.parse(localStorage.getItem('lastNames'));
 let savedAge = parseInt(JSON.parse(localStorage.getItem('age')));
 let savedEmail = JSON.parse(localStorage.getItem('email'));
 let savedTelephone = JSON.parse(localStorage.getItem('telephone'));
+let savedImage = localStorage.getItem('image');
 
 const verificar = () => {
     if (savedName !== null) {
@@ -22,6 +25,8 @@ const verificar = () => {
         age.value = savedAge;
         email.value = savedEmail;
         telephone.value = savedTelephone;
+        image.src = "data:image/png;base64," + savedImage;
+
     }
 }
 
@@ -47,12 +52,14 @@ const guardarCambios = () => {
         datosUsuario.age = age.value;
         datosUsuario.email = email.value;
         datosUsuario.telephone = telephone.value;
+        datosUsuario.image = getBase64Image(image);
 
         localStorage.setItem('name', JSON.stringify(datosUsuario.name));
         localStorage.setItem('lastNames', JSON.stringify(datosUsuario.lastNames));
         localStorage.setItem('age', JSON.stringify(datosUsuario.age));
         localStorage.setItem('email', JSON.stringify(datosUsuario.email));
         localStorage.setItem('telephone', JSON.stringify(datosUsuario.telephone));
+        localStorage.setItem('image', getBase64Image(image));
 
         return swal({
             title: "Cambios guardados",
@@ -65,6 +72,43 @@ const guardarCambios = () => {
     }
 }
 
+/*Read image as URL*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('imgPerfil')
+                .setAttribute('src', e.target.result)
+                .width(150)
+                .height(200);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+/*While image has not been set*/
+
+const setDefaultImage = () => {
+    let image = document.getElementById('imgPerfil');
+    if (image.getAttribute('src') === '') {
+        image.setAttribute('src', 'https://lh3.googleusercontent.com/proxy/_ANnvQRzBG0Nf5F6xPyeoEDxWW7Otnz2h1Ahr7HCmYyP3TJ0juDEIDd177j_fBXwbHSUlC_f14GeHYEMB9_5eej9uPGSMasEiF5PxDPFUcJJrVP4')
+    }
+}
+
+/*Transformations to save image in LocalStorage */
+const getBase64Image = (img) => {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
 
 
 
@@ -72,7 +116,7 @@ const guardarCambios = () => {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-
+    setDefaultImage();
     verificar();
 
     document.getElementById('saveChanges').addEventListener('click', () => {
